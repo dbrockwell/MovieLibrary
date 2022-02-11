d25db979ac9851037c2d0e7c627d8267332f8c57
David Brockwell <dbrockwell@my.wctc.edu>
2022-02-11 16:34:28 -0600

    Added nlog, and view movies

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using NLog.Web;
 
 namespace MovieLibrary
 {
@@ -6,7 +9,38 @@ namespace MovieLibrary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+
+            logger.Info("Program started");
+            
+            Console.WriteLine("Enter 1 to view movies");
+            Console.WriteLine("Enter 2 to add data");
+            Console.WriteLine("Enter any other key to exit");
+
+            string choose = Console.ReadLine();
+
+            var file = "movies.csv";
+
+            if (choose == "1") {
+                if (File.Exists(file))
+                {
+                    StreamReader sr = new StreamReader(file);
+                    while(!sr.EndOfStream) {
+                        string line = sr.ReadLine();
+                        string[] movie = line.Split(',');
+                        movie = Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                        Console.WriteLine($"{movie[0],-10}{movie[1],-150}{movie[2]}");
+                    }
+                }
+                else {
+                    logger.Warn("File does not exists. {file}", file);
+                }
+            }
+
+            else if (choose == "2") {
+
+            } 
         }
     }
 }
